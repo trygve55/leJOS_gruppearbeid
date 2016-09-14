e202d1edef189a98b373578051d475415033f559
trygve55 <trygve55@gmail.com>
2016-09-14 12:09:17 +0200

    Hitratunnel

---
+++ HitraTunnel/HitraTunnel.java
@@ -0,0 +1,54 @@
+class Clean {
+	public Clean() {
+
+	}
+
+	public boolean isUp() {
+
+	}
+
+	public void setPos(boolean) { //up = true, down = false
+
+	}
+
+	public void startClean() {
+
+	}
+
+	public void stopClean() {
+
+	}
+}
+
+class Drive {
+	//differencial pilot
+
+	public Drive(String LeftM, String RightM) {  //m1 = leftM
+
+	}
+
+	public void stop() {
+
+	}
+
+	public void start(int i) { //i = speed
+
+	}
+
+	public void travel(int i) { //i = cm
+
+	}
+
+	public void rotate(int i) { //i = deg
+
+	}
+
+}
+
+class HitraTunnel {
+
+	public static void main(String[] args) {
+
+	}
+
+}
