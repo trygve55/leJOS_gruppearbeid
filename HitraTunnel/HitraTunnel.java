7c5db605c676a3834553fa5770bd1c0848d031de
tinelipe <TineElisabeth@Tines-MacBook-Pro.local>
2016-09-14 12:46:22 +0200

    colorsensor
    Kopierte greier fra eksempelkode og la inn alt som hadde med
    fargesensoren å gjøre i main class.

--- HitraTunnel/HitraTunnel.java
+++ HitraTunnel/HitraTunnel.java
@@ -1,3 +1,11 @@
+import lejos.hardware.motor.*;
+import lejos.hardware.sensor.EV3TouchSensor;
+import lejos.hardware.sensor.EV3ColorSensor;
+import lejos.hardware.port.Port;
+import lejos.hardware.Brick;
+import lejos.hardware.BrickFinder;
+import lejos.robotics.SampleProvider;
+
 class Clean {
 	public Clean() {
 
@@ -48,6 +56,41 @@ class Drive {
 class HitraTunnel {
 
 	public static void main(String[] args) {
+
+		// Definerer sensorer:
+		Brick brick = BrickFinder.getDefault();
+	    Port s1 = brick.getPort("S1"); // fargesensor
+	 	Port s2 = brick.getPort("S2"); // trykksensor
+
+		EV3ColorSensor fargesensor = new EV3ColorSensor(s1); // ev3-fargesensor
+		SampleProvider fargeLeser = fargesensor.getMode("RGB");  // svart = 0.01..
+		float[] fargeSample = new float[fargeLeser.sampleSize()];  // tabell som innholder avlest verdi
+
+		// Beregn verdi for svart
+		int svart = 0;
+		for (int i = 0; i<100; i++){
+			fargeLeser.fetchSample(fargeSample, 0);
+			svart += fargeSample[0]* 100;
+		}
+		svart = svart / 100 + 5;
+		System.out.println("Svart: " + svart);
+
+		boolean fortsett = true;
+
+		while (fortsett){ 	// Fortsett s� lenge roboten ikke treffer noe
+		   fargeLeser.fetchSample(fargeSample, 0);
+	       if (fargeSample[0]*100 > svart){   // sjekk sort linje
+	       	  Motor.A.forward();
+	          Motor.B.forward();        // viftearm
+	          Motor.C.stop();  		// snu i  200 millisekund
+	          Thread.sleep(100);
+	          System.out.println("hvit");
+	       } else {
+			   // Kj�r framover
+			   Motor.A.forward();
+			   Motor.C.forward();
+			   System.out.println("svart");
+		   }
 
 	}
 
